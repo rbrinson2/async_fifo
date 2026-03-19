215ec3f7a2c773798bd7b7c8779a669d5238a1b6
rbrin <rbrinson2@gmail.com>
2026-03-19 11:18:45 -0400

    VHDL version started

---
+++ src/fifo.vhdl
@@ -0,0 +1,19 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+use IEEE.MATH_REAL.all;
+
+
+entity fifo is
+    generic (
+        FIFO_DATAWIDTH  : NATURAL := 32;
+        FIFO_DEPTH      : NATURAL := 16;
+        FIFO_PTR        : NATURAL := 4
+    );
+    port (
+        clk : in STD_ULOGIC;
+        rst : in STD_ULOGIC
+    );
+end entity fifo;
