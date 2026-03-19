98b0c937534732636a6aed6f0c8151bb5572ce35
rbrin <rbrinson2@gmail.com>
2026-03-19 15:01:21 -0400

    VHDL tweaks

---
+++ testbench/fifo_tb.vhdl
@@ -0,0 +1,17 @@
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+entity fifo_tb is
+end entity fifo_tb;
+
+
+architecture rtl of fifo_tb is
+    
+begin
+
+
+end architecture rtl;
